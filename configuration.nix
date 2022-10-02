54b49aab244419a1acff753c10107f35c151ddf7
Christian <blankj.me@gmail.com>
2022-10-02 20:00:21 +0200

    Feature: AwesomeWM.git Overlay added

--- configuration.nix
+++ configuration.nix
@@ -123,6 +123,14 @@
 
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
+  # Awesomewm.git included in Overlay 
+  nixpkgs.overlays = [
+		(builtins.getFlake "github:fortuneteller2k/nixpkgs-f2k").overlays.default
+		#(import (builtins.fetchTarball {
+		#	url = https://github.com/nix-community/neovim-nightly-overlay/archive/master.tar.gz;
+		#}))
+ 	];
+
 
   # List packages installed in system profile. To search, run:
   # $ nix search wget
